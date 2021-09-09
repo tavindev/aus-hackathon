8cb5075b7848378ada6466449eb8a76146835849
gustavo-dev <gvwchevrand@gmail.com>
2021-09-09 10:59:00 -0300

    feat(medication): added Medication List animations

--- components/UserPage/Medication.tsx
+++ components/UserPage/Medication.tsx
@@ -3,22 +3,47 @@ import React, { useState } from 'react';
 
 interface MedicationProps {
   name: string;
+  description: string;
 }
 
-export const Medication: React.FC<MedicationProps> = ({ name }) => {
+export const Medication: React.FC<MedicationProps> = ({
+  name,
+  description,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
     <motion.div
-      layout
+      layout="position"
       onClick={toggleOpen}
-      className="h-10 w-1/3 bg-gray-100 overflow-hidden rounded"
+      className="w-1/2 2xl:w-1/3 bg-gray-100 overflow-hidden rounded cursor-pointer p-3 box-border"
     >
-      <div className="flex items-center h-full mx-4">
+      <motion.div className="flex items-center justify-between h-8">
         <p className="text-sm font-bold">{name}</p>
-      </div>
+        <div
+          className={`${
+            isOpen ? 'bg-red-500' : 'bg-green-500'
+          } text-sm text-white font-bold p-1 rounded`}
+        >
+          {isOpen ? 'Fechar' : 'Abrir'}
+        </div>
+      </motion.div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className="flex items-center text-xs">
+              <p>{description}</p>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.div>
   );
 };
--- components/UserPage/MedicationList.tsx
+++ components/UserPage/MedicationList.tsx
@@ -1,21 +1,39 @@
 import { Medication } from 'components/UserPage/Medication';
-import { AnimateSharedLayout, motion } from 'framer-motion';
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
+import React, { useState } from 'react';
+
+const items = [
+  {
+    name: 'Paracetamol',
+    description: 'Dummy description',
+  },
+  {
+    name: 'Novalgina',
+    description: 'Tomar de 8 em 8 horas',
+  },
+  {
+    name: 'Soro',
+    description: 'Tomar de noite',
+  },
+];
 
 export const MedicationList: React.FC = () => {
   return (
-    <div className="p-8 h-full overflow-hidden w-full">
-      <div>
+    <div className="flex flex-col p-6 h-full overflow-hidden w-full">
+      <div className="mb-4">
         <h2 className="text-lg font-bold">Medicamentos</h2>
       </div>
-      <div className="mt-5">
+      <div className="w-full flex-grow">
         <AnimateSharedLayout>
           <motion.div
             layout
-            className="flex gap-4 min-h-full overflow-y-scroll"
+            className="flex flex-col flex-wrap h-full overflow-y-hidden overflow-x-scroll gap-2"
           >
-            <Medication name="Paracetamol" />
-            <Medication name="Novalgina" />
-            <Medication name="Soro" />
+            {items.map(({ name, description }, i) => {
+              return (
+                <Medication key={i} name={name} description={description} />
+              );
+            })}
           </motion.div>
         </AnimateSharedLayout>
       </div>
