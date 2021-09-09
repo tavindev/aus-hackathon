4fe62887d3f003411509b470bb4ce90e6f4aab30
gustavo-dev <gvwchevrand@gmail.com>
2021-09-09 13:35:36 -0300

    feat: added Loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="loader w-24 h-24 rounded-full"></div>
+    </div>
+  );
+};
