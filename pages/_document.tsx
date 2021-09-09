68195a6a65d232cb9ec78dadf2e499af97332de2
gustavo-dev <gvwchevrand@gmail.com>
2021-09-08 21:39:21 -0300

    feat(document.tsx): dialogflow integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+          {/** @ts-ignore */}
+          <df-messenger
+            intent="WELCOME"
+            chat-title="DoutorVirtual"
+            agent-id="48fde575-b070-448e-93da-2519a924505f"
+            language-code="pt-br"
+          >
+            {/** @ts-ignore */}
+          </df-messenger>
         </Head>
         <body>
           <Main />
