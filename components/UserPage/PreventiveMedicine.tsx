fc0e6e2cc249a2687378e5559aa6391fa9b4625f
gustavo-dev <gvwchevrand@gmail.com>
2021-09-09 11:06:20 -0300

    feat: added new PreventiveMedicine component

---
+++ components/UserPage/PreventiveMedicine.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const PreventiveMedicine: React.FC = () => {
+  return (
+    <div className="flex flex-col h-full w-full">
+      <div className="pl-4 pt-4 h-12 overflow-hidden">
+        <h2 className="text-xl font-bold">Medicina Preventiva</h2>
+      </div>
+    </div>
+  );
+};
