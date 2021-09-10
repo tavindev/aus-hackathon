d2633fd4e82e62d110b9b2af0b11fd9abba39e62
gustavo-dev <gvwchevrand@gmail.com>
2021-09-10 11:55:06 -0300

    feat(dialogflow): removed dialogflow

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,16 +14,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-          {/** @ts-ignore */}
-          <df-messenger
-            intent="WELCOME"
-            chat-title="DoutorVirtual"
-            agent-id="e2f785fc-9c78-468d-b107-c91cb88d4178"
-            language-code="pt-br"
-          >
-            {/** @ts-ignore */}
-          </df-messenger>
         </Head>
         <body>
           <Main />
